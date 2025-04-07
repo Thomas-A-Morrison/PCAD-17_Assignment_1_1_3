06eab85f90743b8902457e87f62f5ffdbf232e53
Thoma <Thoma@LAPTOP-LU9AH37M>
2025-04-07 17:42:26 -0400

    Add project files.

---
+++ PCAD-17_Assignment_1_1_3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Program:    PCAD_Project_1.1.3
+// Date:       7 APR 2025
+// Programmer: Thomas A. Morrison
+
+using System;
+using System.Globalization;
+namespace PCAD_Project_1_1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the first number: ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please enter the second number: ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            double quotientNumber = firstNumber / secondNumber;
+            double remainderNumber = firstNumber % secondNumber;
+
+            Console.WriteLine($"The quotient of {firstNumber} and {secondNumber} is {quotientNumber}.");
+            Console.WriteLine($"The modulus of {firstNumber} and {secondNumber} is {remainderNumber}.");
+        }
+    }
+}
